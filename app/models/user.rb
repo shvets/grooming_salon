110722b1cf5564643f7af0a926cdd10497d7d088
Alexander Shvets <oshvets@ashvetsmacpro.local>
2009-10-24 17:34:42 -0400

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,18 @@
 #  updated_at    :datetime        
 #
 
+class ActiveRecord::Base
+  def self.encrypt(*attr_names)
+    encrypter = Encrypter.new(attr_names)
+
+    before_save encrypter
+    after_save  encrypter
+    after_find  encrypter
+
+    define_method(:after_find) { }
+  end
+end
+
 class User < ActiveRecord::Base
   encrypt(:email)
 
