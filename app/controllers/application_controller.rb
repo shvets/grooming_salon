3be6f6ac5624b3ad7236d4cb2ae7035e2f903d13
Alexander Shvets <oshvets@ashvetsmacpro.local>
2009-10-25 21:00:41 -0400

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,11 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery :secret => 'f599262b5622ea5038a3a07071187bd0'
+  protect_from_forgery
+  #:secret => 'f599262b5622ea5038a3a07071187bd0'
 
   # Pick a unique cookie name to distinguish our session data from others'
-  session :session_key => '_userapp_session_id'
+  session :session_key => '_grooming_salon_session'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
